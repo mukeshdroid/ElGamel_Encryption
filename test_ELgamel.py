6c1bb8de8d1f5f293b1d84855987c18a6ceea3ef
mukeshdroid <58639831+mukeshdroid@users.noreply.github.com>
2023-06-06 20:56:14 +0545

    Minor Changes

--- test_ELgamel.py
+++ test_ELgamel.py
@@ -4,11 +4,10 @@ from Elgamel.lib import intEncodingLen, charEncodingLen, num2string, string2num
 from Elgamel.encrypt import encrypt_msg, encrypt, encrypt_msg_numerical
 from Elgamel.decrypt import decrypt_msg, decrypt, decrypt_msg_numerical
 
-
 blocksize = 4
 
 
-message = 'I LOVE PIKU'
+message = 'HELLO'
 
 start = time.perf_counter_ns()
 alice_private_key, alice_public_key = keyGen(blocksize)
